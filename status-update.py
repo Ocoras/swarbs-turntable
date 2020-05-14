a7d29f9afb0a8efcc80b51b4356291e6db954951
David Swarbrick <david.swarbrick9@gmail.com>
2020-05-14 01:06:53 +0100

    Added NTS Support

--- status-update.py
+++ status-update.py
@@ -36,9 +36,7 @@ def _update_status(filled_template, imagedata):
     )
 
 
-def update_status_soundcloud_mix(url, auth, artist=None, title=None, debug=False):
-    # template =
-
+def update_status_soundcloud_mix(url, artist=None, title=None):
     r = requests.get(url)
     soup = BeautifulSoup(r.content, "html.parser")
 
@@ -47,26 +45,122 @@ def update_status_soundcloud_mix(url, auth, artist=None, title=None, debug=False
         title = foundtitle.string
     if artist is None:
         artist = foundartist.string
-
     year = soup.find("time").string[:4]
-    img_link = soup.find("img")["src"]
-
-    imagedata = requests.get(img_link).content
     filled_template = template.format(artist=artist, title=title, year=year, url=url)
 
-    if debug:
-        print(filled_template)
-    else:
+    print("About to Tweet: ", filled_template)
+    ok = input("Does this look right? (y/n) ")
+
+    if ok == "y":
+        img_link = soup.find("img")["src"]
+        imagedata = requests.get(img_link).content
+        print("Tweeting: ", filled_template)
         _update_status(filled_template, imagedata)
 
 
+def _nts_template_filler(
+    results, channel, artist=None, title=None, time="now", swap=False
+):
+    """Handle NTS Shows that don't have a title"""
+    bt = results[channel - 1][time]["broadcast_title"]
+
+    if bt.find("W/") > 0:
+        # If a big W in there, split on that
+        broadcast_title = bt.split("W/")
+    else:
+        # Otherwise try and split on little w
+        broadcast_title = bt.split("w/")
+
+    if bt.find("(R)") > 0:
+        # ToDo: Handle Re-records, and remove (R) symbol
+        print("This is a re-record, could try and fetch original time here")
+        year = "N/A"
+    else:
+        year = "LIVE"
+    if len(broadcast_title) == 2:
+        # A W was there somewhere - reformat to match template
+        if title is None:
+            title = broadcast_title[0].strip()
+        if artist is None:
+            artist = broadcast_title[1].strip()
+
+        if swap:
+            # Assuming deep copy not needed
+            s = title
+            title = artist
+            artist = s
+    elif len(broadcast_title) == 1:
+        # Single dj title, so set the title to NTS + date
+        if artist is None:
+            artist = broadcast_title[0].strip()
+        if title is None:
+            t = results[channel - 1][time]["start_timestamp"]
+            yr = t[2:4]
+            month = t[5:7]
+            day = t[8:10]
+            title = "NTS " + day + "/" + month + "/" + yr
+
+    return template.format(
+        artist=artist,
+        title=title,
+        year=year,
+        url="https://www.nts.live/" + str(channel),
+    )
+
+
+def _nts_check(filled_template, prev_answers=(False, False, False)):
+    print("About to Tweet: ", filled_template)
+    ok = input("Does this look right? ([y]es/[n]ext/[s]wap) ")
+
+    if ok == "y":
+        return (True, False, False)
+    elif ok == "n":
+        return (prev_answers[0], not prev_answers[1], prev_answers[2])
+    elif ok == "s":
+        return (prev_answers[0], prev_answers[1], not prev_answers[2])
+    else:
+        return (False, False, False)
+
+
+def update_status_ntslive(channel, artist=None, title=None):
+    r = requests.get("https://www.nts.live/api/v2/live")
+    results = r.json()["results"]
+    print("Data for Channel ", results[channel - 1]["channel_name"])
+
+    filled_template = _nts_template_filler(results, channel, artist, title)
+    flags = _nts_check(filled_template)
+    time = "now"
+    # 0 = y, 1 = next show, 2 = swap order of show/artist
+    while flags[0] or flags[1] or flags[2]:
+        if flags[0]:
+            img_link = results[channel - 1][time]["embeds"]["details"]["media"][
+                "picture_large"
+            ]
+            imagedata = requests.get(img_link).content
+            _update_status(filled_template, imagedata)
+            flags = [False, False, False]  # Exit loop
+        else:
+            if flags[1]:
+                time = "next"
+            else:
+                time = "now"
+            swap = flags[2]
+            filled_template = _nts_template_filler(
+                results, channel, artist, title, time, swap
+            )
+            flags = _nts_check(filled_template, flags)
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument("url", help="url for Soundcloud mix")
+parser.add_argument("url", help="URL to grab data from. (Soundcloud/NTS)")
 parser.add_argument("-a", "--artist", default=None, help="Artist override for tweet")
 parser.add_argument("-t", "--title", default=None, help="Title override for tweet")
-parser.add_argument("-d", "--debug", action="store_true", help="Debug - Don't print")
+parser.add_argument("-n", "--nts", default=1, help="NTS Channel (1/2)")
 args = parser.parse_args()
 
 if "soundcloud" in args.url:
-    print("Soundcloud Link Selected")
-    update_status_soundcloud_mix(args.url, auth, args.artist, args.title, args.debug)
+    print("Source: Soundcloud")
+    update_status_soundcloud_mix(args.url, args.artist, args.title)
+elif "nts.live" in args.url:
+    print("Source: NTS Live")
+    update_status_ntslive(int(args.nts), args.artist, args.title)
